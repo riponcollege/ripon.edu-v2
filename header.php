3e2f601174252905e993523959d03aec9e66d20f
James Pederson <james@jpederson.com>
2020-11-20 11:40:17 -0600

    Update faculty info background styles.

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <?php wp_head(); ?>
 
 <?php if ( is_search() ) { ?><meta name="robots" content="noindex" /><?php } ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=169" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=170" rel="stylesheet" type="text/css">
 
 <!-- Facebook Pixel Code -->
 <script>
