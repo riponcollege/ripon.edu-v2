c8c43380aaa24a41f1d858eef0b3cb3a126939a3
James Pederson <james@jpederson.com>
2021-01-18 10:12:07 -0600

    Implement areas of study main page design.

--- page-academics.php
+++ page-academics.php
@@ -33,43 +33,51 @@ get_header();
 			<div class="wrap group two-column academics">
 			
 				<div class="area-legend">
-					<table cellspacing=0 cellpadding=3>
-						<tr>
-							<td><span class="ma">MA</span></td>
-							<td>
+					<div class="area-legend-header bg-red-dark">
+						<div class="icon">
+							<img src="<?php bloginfo( 'template_url' ); ?>/img/icon-area-legend.webp" />
+						</div>
+						<div class="intro">
+							<p>At Ripon College, you choose your path. In fact, Ripon offers students the chance to develop a self-designed major (listed below) with the guidance of a faculty advisor. Recent self-design majors have included: Film Studies, Criminal Justice and Journalism. So, if you’re undecided, maybe this is the choice for you!</p>
+						</div>
+					</div>
+					<dl>
+						<div class="term">
+							<dt><span class="ma">MA</span></dt>
+							<dd>
 								Major
 								<span class="term-description"><?php echo term_description( 502, "area_cat" ); ?></span>
-							</td>
-						</tr>
-						<tr>
-							<td><span class="mi">MI</span></td>
-							<td>
+							</dd>
+						</div>
+						<div class="term">
+							<dt><span class="mi">MI</span></dt>
+							<dd>
 								Minor
 								<span class="term-description"><?php echo term_description( 503, "area_cat" ); ?></span>
-							</td>
-						</tr>
-						<tr>
-							<td><span class="pa">PA</span></td>
-							<td>
+							</dd>
+						</div>
+						<div class="term">
+							<dt><span class="pa">PA</span></dt>
+							<dd>
 								Pre-Professional Advising
 								<span class="term-description"><?php echo term_description( 5297, "area_cat" ); ?></span>
-							</td>
-						</tr>
-						<tr>
-							<td><span class="tc">TC</span></td>
-							<td>
+							</dd>
+						</div>
+						<div class="term">
+							<dt><span class="tc">TC</span></dt>
+							<dd>
 								Teaching Certification
 								<span class="term-description"><?php echo term_description( 5316, "area_cat" ); ?></span>
-							</td>
-						</tr>
-						<tr>
-							<td><span class="dd">DD</span></td>
-							<td>
+							</dd>
+						</div>
+						<div class="term">
+							<dt><span class="dd">DD</span></dt>
+							<dd>
 								Dual Degree<br>
 								<span class="term-description"><?php echo term_description( 5552, "area_cat" ); ?></span>
-							</td>
-						</tr>
-					</table>
+							</dd>
+						</div>
+					</dl>
 				</div>
 				<div class="group area-tabs">
 					<?php list_area_category() ?>
