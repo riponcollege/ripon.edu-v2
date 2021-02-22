e79c1d73a9b60c1ade8bc3c6b233b0cf00320bf1
James Pederson <james@jpederson.com>
2021-02-21 20:29:08 -0600

    Adjust alum-listing right margin to line things up.

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <?php wp_head(); ?>
 
 <?php if ( is_search() ) { ?><meta name="robots" content="noindex" /><?php } ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=213" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=214" rel="stylesheet" type="text/css">
 
 <!-- Facebook Pixel Code -->
 <script>
