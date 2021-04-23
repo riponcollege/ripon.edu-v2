24a5ccf8684608916e12e4ea56ac0f922a3199b0
James Pederson <james@jpederson.com>
2021-04-23 17:10:46 -0500

    Update bio page sidebar, etc.

--- single-people.php
+++ single-people.php
@@ -31,8 +31,8 @@ $courses = get_cmb_value( "faculty_courses" );
 				<div class="person-info-inner">
 					<h2><?php the_title(); ?></h2>
 					<h4 class="person-title"><?php print get_cmb_value( "person_title" ); ?></h4>
-					<p><a href="mailto:<?php print get_cmb_value( "person_email" ); ?>"><?php print get_cmb_value( "person_email" ); ?></a><br>
-					<?php print get_cmb_value( "person_phone" ); ?></p>
+					<p><a href="mailto:<?php print get_cmb_value( "person_email" ); ?>"><?php print get_cmb_value( "person_email" ); ?></a></p>
+					<p><?php print get_cmb_value( "person_phone" ); ?></p>
 					<?php if ( has_cmb_value( "person_office" ) ) { ?><p>Office: <?php print get_cmb_value( "person_office" ); ?></p><?php } ?>
 					<?php if ( has_cmb_value( "person_website" ) ) { ?><p><a href='" . get_cmb_value( "person_website" ) . "' target='_blank'>Website</a></p><?php } ?>
 					<p class="cv-link"><a href="<?php show_cmb_value( "person_cv" ); ?>" class="btn red-dark">View CV/Resume</a></p>
