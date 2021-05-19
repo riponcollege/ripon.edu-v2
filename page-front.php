61e29e5876abe421f86a616f83fdd4578a83d561
James Pederson <james@jpederson.com>
2021-05-19 09:32:12 -0500

    Adjust homepage to accommodate alumni site.

--- page-front.php
+++ page-front.php
@@ -6,19 +6,28 @@ Template Name: Front
 
 get_header();
 
-?>
-
+if ( !is_alumni() ) {
+	?>
 <div class="front-title first">
 	<h2>Explore Ripon</h2>
 </div>
-<?php the_phototiles(); ?>
+	<?php 
+	the_phototiles(); 
+}
+?>
 
-<div class="front-title">
+<div class="front-title<?php print ( is_alumni() ? ' first' : '' ); ?>">
 	<h2>News & Events</h2>
 </div>
 <div class="front-columns">
 	<div class="front-articles">
-		<?php print do_shortcode( '[articles /]' ); ?>
+		<?php 
+		if ( is_alumni() ) {
+			print do_shortcode( '[articles feed="https://ripon.edu/category/alumni/feed/" /]' ); 
+		} else {
+			print do_shortcode( '[articles /]' ); 
+		}
+		?>
 	</div>
 	<div class="front-events">
 		<?php print do_shortcode( '[events limit=4 /]' ); ?>
