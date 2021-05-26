4ff69c76b02bbfa0c680e8f4060f5972579616e3
James Pederson <james@jpederson.com>
2021-05-25 19:55:52 -0500

    Adjust event display.

--- single-event.php
+++ single-event.php
@@ -10,44 +10,38 @@ get_header();
 			?>
 	<div id="primary" class="wrap group">
 		<div class="event content-wide" role="main">
-			<div class="two-third entry-content">
-			<?php
-				global $post;
-				$post_id = $post->ID;
-				?>
-				<h1><?php the_title(); ?></h1>
-				<?php the_content(); ?>
-				<hr>
-				<h2>Upcoming Events</h2>
+			<div class="group">
+				<div class="two-third entry-content">
 				<?php
-				print do_shortcode( '[events /]' );
-				?>
-		 	</div>
-		 	<div class="third">
-				<?php the_post_thumbnail(); ?>
-		 		<p>&nbsp;</p>
-				<?php
-					// display credit union name
+					global $post;
+					$post_id = $post->ID;
+					?>
+					<h1><?php the_title(); ?></h1>
+					<?php
+					// display event date
 					if ( has_cmb_value( 'event_start' ) && has_cmb_value( 'event_end' ) ) {
 						$start = get_cmb_value( 'event_start' );
 						$end = get_cmb_value( 'event_end' );
 						if ( date( 'Ymd', $start ) != date( 'Ymd', $end ) ) {
-							print "<p>" . date( "F jS g:i a", $start ) . " -";
-							print date( "F jS g:i a", $end ) . "</p>";
+							print "<h4>" . date( "F jS g:i a", $start ) . " - ";
+							print date( "F jS g:i a", $end ) . "</h4>";
 						} else {
-							print "<h4>" . date( "F jS", $start ) . "</h4>";
-							print "<p>" . date( "g:i a", $start );
-							print " - " . date( "g:i a", $end ) . "</p>";
+							print "<h4>" . date( "F jS", $start ) . ': ' . date( "g:i a", $start );
+							print " - " . date( "g:i a", $end ) . "</h4>";
 						}
 					}
-
-					// display the event duration.
-					if ( has_cmb_value( 'event_start' ) && has_cmb_value( 'event_end' ) ) {
-						print "<p><strong>Duration:</strong> " . duration( get_cmb_value( 'event_start' ), get_cmb_value( 'event_end' ) ) . "</p>";
-					}
-
-				?>
-		 	</div>
+					?>
+					<?php the_content(); ?>
+					<hr>
+					<h2>Upcoming Events</h2>
+					<?php
+					print do_shortcode( '[events /]' );
+					?>
+			 	</div>
+			 	<div class="third event-image">
+					<?php the_post_thumbnail(); ?>
+			 	</div>
+			 </div>
 		</div>
 	</div>
 
