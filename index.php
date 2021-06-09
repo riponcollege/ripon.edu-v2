b48718a40a7729f7abc8db54baec795a91c15440
James Pederson <james@jpederson.com>
2021-06-09 10:24:52 -0500

    Remove padding from the left of gravity forms field sections.

--- index.php
+++ index.php
@@ -55,23 +55,23 @@ if ( $paged > 0 ) {
 	</div>
 	<hr />
 	<div class="article-cards blog-listing">
-<?php
-
-while ( have_posts() ) : the_post();
-	?>
-	<div class="entry">
-		<div class="thumbnail">
-			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-		</div>
-		<div class="entry-inner">
-			<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-			<?php the_excerpt(); ?>
-		</div>
-	</div>
 	<?php
-endwhile;
 
-?>
+	while ( have_posts() ) : the_post();
+		?>
+		<div class="entry">
+			<div class="thumbnail">
+				<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+			</div>
+			<div class="entry-inner">
+				<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+				<?php the_excerpt(); ?>
+			</div>
+		</div>
+		<?php
+	endwhile;
+
+	?>
 	</div>
 
 	<div class="pagination">
