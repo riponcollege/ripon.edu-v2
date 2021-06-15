e5441beae2450010046a5222d8cc2dc8269e710c
James Pederson <james@jpederson.com>
2021-06-15 13:35:40 -0500

    Update footer for alumni only.

--- footer.php
+++ footer.php
@@ -17,9 +17,15 @@
 		<footer>
 			
 			<div class="address">
+				<?php if ( is_alumni() ) { ?>
+				<strong>Ripon College</strong><br>
+				300 W. Seward St. | Ripon, WI 54971<br>
+				920-738-8126 | <a href="mailto:alumni@ripon.edu">alumni@ripon.edu</a>
+				<?php } else { ?>
 				<strong>Ripon College</strong><br>
 				300 W. Seward St.<br>
 				Ripon, WI 54971
+				<?php } ?>
 			</div>
 
 			<?php if ( !has_call_to_action() ) { ?>
@@ -27,8 +33,10 @@
 			<?php } ?>
 
 			<div class="social">
+				<?php if ( !is_alumni() ) { ?>
 				<a href="tel:9207488115"><img src="<?php bloginfo('template_url') ?>/img/social-phone.webp"></a>
 				<a href="mailto:adminfo@ripon.edu"><img src="<?php bloginfo('template_url') ?>/img/social-email.webp"></a>
+				<?php } ?>
 				<a href="http://www.facebook.com/ripon.college"><img src="<?php bloginfo('template_url') ?>/img/social-facebook.webp"></a>
 				<a href="http://www.linkedin.com/groups?home=&gid=4646327&trk=anet_ug_hm"><img src="<?php bloginfo('template_url') ?>/img/social-linkedin.webp"></a>
 				<a href="https://twitter.com/riponcollege"><img src="<?php bloginfo('template_url') ?>/img/social-twitter.webp"></a>
