4e45f24f140ef2e482575c45a60030bc1ea9d974
James Pederson <james@jpederson.com>
2021-06-15 12:01:44 -0500

    Hide cv-link if it's not going to be populated.

--- single-people.php
+++ single-people.php
@@ -35,7 +35,7 @@ $courses = get_cmb_value( "faculty_courses" );
 					<p><?php print get_cmb_value( "person_phone" ); ?></p>
 					<?php if ( has_cmb_value( "person_office" ) ) { ?><p>Office: <?php print get_cmb_value( "person_office" ); ?></p><?php } ?>
 					<?php if ( has_cmb_value( "person_website" ) ) { ?><p><a href='" . get_cmb_value( "person_website" ) . "' target='_blank'>Website</a></p><?php } ?>
-					<p class="cv-link"><a href="<?php show_cmb_value( "person_cv" ); ?>" class="btn red-dark">View CV/Resume</a></p>
+					<?php if ( has_cmb_value( "person_cv" ) ) { ?><p class="cv-link"><a href="<?php show_cmb_value( "person_cv" ); ?>" class="btn red-dark">View CV/Resume</a></p><?php } ?>
 				</div>
 			</div>
 		</div>
