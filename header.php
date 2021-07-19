2ab223ace88f89462d2fd79790b260e4b5ebf3c2
James Pederson <james@jpederson.com>
2021-07-19 17:12:38 -0500

    Bump css and js, and remove some excess console.log code from video.js.

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <?php wp_head(); ?>
 
 <?php if ( is_search() ) { ?><meta name="robots" content="noindex" /><?php } ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=259" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=260" rel="stylesheet" type="text/css">
 
 <!-- Facebook Pixel Code -->
 <script>
--- js/src/video.js
+++ js/src/video.js
@@ -8,14 +8,6 @@ jQuery(document).ready(function($){
 		var window_width = $(window).width();
 		var target_width = 1920 * ( 1080 / window_height );
 		var target_margin = ( target_width - window_width ) / 2;
-
-		//$('.video-showcase-container').height( window_height );
-
-		/*
-		console.log( target_margin );
-
-		$('.video-showcase-container .video-showcase').css( 'margin-left', target_margin * -1 ).css( 'margin-right', target_margin * -1 );
-		*/
 	}
 
 	if ( $('.video-showcase-container') ) {
