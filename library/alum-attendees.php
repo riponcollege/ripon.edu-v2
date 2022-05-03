13fcb5d470493618aaed3c8251ea010b9cedcf95
James Pederson <james@jpederson.com>
2022-05-02 20:36:12 -0500

    Adjust the way names are displayed to show last name first (makes it clearer), and remove limit on entries (up to 400 names).

--- library/alum-attendees.php
+++ library/alum-attendees.php
@@ -9,7 +9,11 @@ function alum_attendees() {
 		'is_numeric' => false
 	);
 
-	$entries = GFAPI::get_entries( 261, null, $sorting );
+	$paging = array( 
+		'page_size' => 400
+	);
+
+	$entries = GFAPI::get_entries( 261, null, $sorting, $paging );
 
 	// return print_r( $entries, 1 );
 
@@ -17,7 +21,7 @@ function alum_attendees() {
 
 	foreach ( $entries as $entry ) {
 		if ( $entry[120] == 'Yes' ) {
-			$attendee_list .= '<li><strong>' . $entry['43.3'] . ' ' . $entry['43.6'] . '</strong> (' . $entry['44'] . ')' . ( !empty( $entry['103.3'] ) ? '<br>w/ ' . $entry['103.3'] . ' ' . $entry['103.6'] . ( !empty( $entry['104'] ) ? ' (' . $entry['104'] . ') ' : '' ) : '' ) . '</li>';
+			$attendee_list .= '<li><strong>' . $entry['43.6'] . ', ' . $entry['43.3'] . '</strong> (' . $entry['44'] . ')' . ( !empty( $entry['103.3'] ) ? '<br>w/ ' . $entry['103.6'] . ', ' . $entry['103.3'] . ( !empty( $entry['104'] ) ? ' (' . $entry['104'] . ') ' : '' ) : '' ) . '</li>';
 		}
 	}
 
