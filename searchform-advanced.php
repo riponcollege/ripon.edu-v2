6bbee2339c08971622f52858934bbf12660695b7
James Pederson <james@jpederson.com>
2024-06-05 15:32:33 +0000

    Fix array error on advanced search form.

--- searchform-advanced.php
+++ searchform-advanced.php
@@ -1,11 +1,24 @@
+<?php
+if ( isset( $_REQUEST['post_type'] ) ) {
+	if ( is_string( $_REQUEST['post_type'] ) ) {
+		$post_type = array(
+			$_REQUEST['post_type']
+		);
+	} else {
+		$post_type = $_REQUEST['post_type'];
+	}
+} else {
+	$post_type = array();
+}
+?>
 <form role="search" method="get" id="searchform" class="searchform" action="/" _lpchecked="1">
 	<label><span>Search Term:</span> <input type="text" value="<?php print ( isset( $_REQUEST['s'] ) ? htmlspecialchars( $_REQUEST['s'] ) : '' ); ?>" name="s" id="s" placeholder="Search"></label>
 	<?php if ( is_ripon() ) { ?>
-	<label><input type="checkbox" name="post_type[]" value="page"<?php print ( isset( $_REQUEST['post_type'] ) ? ( in_array( 'page', $_REQUEST['post_type'] ) ? ' checked="yes"' : '' ) : '' ) ?> /> Pages</label>
-	<label><input type="checkbox" name="post_type[]" value="post"<?php print ( isset( $_REQUEST['post_type'] ) ? ( in_array( 'post', $_REQUEST['post_type'] ) ? ' checked="yes"' : '' ) : '' ) ?> /> Articles</label>
-	<label><input type="checkbox" name="post_type[]" value="event"<?php print ( isset( $_REQUEST['post_type'] ) ? ( in_array( 'event', $_REQUEST['post_type'] ) ? ' checked="yes"' : '' ) : '' ) ?> /> Events</label>
-	<label><input type="checkbox" name="post_type[]" value="people"<?php print ( isset( $_REQUEST['post_type'] ) ? ( in_array( 'people', $_REQUEST['post_type'] ) ? ' checked="yes"' : '' ) : '' ) ?> /> People</label>
-	<label><input type="checkbox" name="post_type[]" value="guide"<?php print ( isset( $_REQUEST['post_type'] ) ? ( in_array( 'guide', $_REQUEST['post_type'] ) ? ' checked="yes"' : '' ) : '' ) ?> /> Research Guides</label>
+	<label><input type="checkbox" name="post_type[]" value="page"<?php print ( in_array( 'page', $post_type ) ? ' checked="yes"' : '' ) ?> /> Pages</label>
+	<label><input type="checkbox" name="post_type[]" value="post"<?php print ( in_array( 'post', $post_type ) ? ' checked="yes"' : '' ) ?> /> Articles</label>
+	<label><input type="checkbox" name="post_type[]" value="event"<?php print ( in_array( 'event', $post_type ) ? ' checked="yes"' : '' ) ?> /> Events</label>
+	<label><input type="checkbox" name="post_type[]" value="people"<?php print ( in_array( 'people', $post_type ) ? ' checked="yes"' : '' ) ?> /> People</label>
+	<label><input type="checkbox" name="post_type[]" value="guide"<?php print ( in_array( 'guide', $post_type ) ? ' checked="yes"' : '' ) ?> /> Research Guides</label>
 	<?php } ?>
 	<label><input type="submit" id="searchsubmit" value="Search" class="btn-arrow"></label>
 </form>
